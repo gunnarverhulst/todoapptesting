f28e478480dcd0abde4c4034b594926aac9b18e8
Gunz <verhulstg@gmail.com>
2024-02-22 09:52:56 +0100

    validate and count tests

--- frontend/src/components/add-todo/validate.test.ts
+++ frontend/src/components/add-todo/validate.test.ts
@@ -1,5 +1,41 @@
 import { isValid } from "./validate.ts";
 
-test("empty input", () => {
+test("whitespace input", () => {
     expect(isValid(" ")).toEqual(false);
 });
+
+test("emtpy input", () => {
+    expect(isValid("")).toEqual(false);
+});
+
+test("valid text input", () => {
+    expect(isValid("text")).toEqual(true);
+});
+
+test("more than 100 characters input", () => {
+    expect(isValid(Array(100).fill('a').join(''))).toEqual(false);
+});
+
+test("more than 100 characters input", () => {
+    expect(isValid(Array(101).fill('a').join(''))).toEqual(false);
+});
+
+test("more than 100 characters input", () => {
+    expect(isValid(Array(99).fill('a').join(''))).toEqual(true);
+});
+
+test.each([
+    ["", false],
+    [" ", false],
+    ["2", true],
+    [createArray(99), true],
+    [createArray(100), false],
+    [createArray(101), false],
+  ])('isValid(%s) -> %s', (a, expected) => {
+    expect(isValid(a)).toBe(expected)
+  })
+
+
+  function createArray(value: number): string{
+    return Array(value).fill('a').join('');
+  }
--- frontend/src/components/footer/count.test.ts
+++ frontend/src/components/footer/count.test.ts
@@ -3,3 +3,33 @@ import { countTodosLeft } from "./count.ts";
 test("dummy test", () => {
     expect(1 + 1).toEqual(2);
 });
+
+const done = {
+    id: "3044efbc-7e54-4751-a96c-e01474caf8a7",
+    description: "Jog around the park",
+    done: true,
+};
+
+const todo = {
+    id: "3044efbc-7e54-4751-a96c-e01474caf8a7",
+    description: "Jog around the park",
+    done: false,
+};
+
+const todo1 = {
+    id: "3044efbc-7e54-4751-a96c-e01474caf8a7",
+    description: "Jog around the park",
+    done: false,
+};
+
+
+test.each([
+    [[], 0],
+    [[done], 0],
+    [[todo], 1],
+    [[done, todo, todo1], 2]
+  ])('countTodosLeft(%o) -> %i', (a, expected) => {
+    expect(countTodosLeft(a)).toBe(expected)
+  })
+
+  
