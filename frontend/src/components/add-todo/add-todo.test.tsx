bbbdea46d52e8d5b2290c489b1358a0ac14a4139
Franky Jr Blondeel <frankyjr@me.com>
2024-02-22 10:50:44 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:frontend/src/components/add-todo/add-todo.test.tsx

--- frontend/src/components/add-todo/add-todo.test.tsx
+++ frontend/src/components/add-todo/add-todo.test.tsx
@@ -1,8 +1,15 @@
 import { render, screen } from "../../test-lib/test-utils";
 import { AddTodoForm } from "./index.tsx";
 
-test("dummy test", () => {
-    render(<AddTodoForm />);
-    const addButton = screen.getByRole("button", { name: /add/i });
-    expect(addButton).toBeDisabled();
+test("user hovers over a button", async () => {
+    const {user} = render(<AddTodoForm />);
+    const addButton = screen.getByRole("button", {name: /Add/i});
+    await user.hover(addButton);
 });
+
+test("user clicks button", async () => {
+    const {user} = render(<AddTodoForm />);
+    const addButton = screen.getByRole("button", {name: /Add/i});
+    await user.click(addButton);
+});
+
