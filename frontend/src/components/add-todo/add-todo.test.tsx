438722cffd82b865c82e1df7c4959923c8016294
Gunz <verhulstg@gmail.com>
2024-02-22 11:32:01 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:frontend/src/components/add-todo/add-todo.test.tsx

--- frontend/src/components/add-todo/add-todo.test.tsx
+++ frontend/src/components/add-todo/add-todo.test.tsx
@@ -10,13 +10,6 @@ test("user hovers over a button", async () => {
 test("user clicks button", async () => {
     const {user} = render(<AddTodoForm />);
     const addButton = screen.getByRole("button", {name: /Add/i});
-    /* await user.click(addButton); */
-    const test = await screen.findAllByLabelText("Description")
-    const keys = Object.keys(test);
-    const nameOfElement = keys[0];
-    console.log(keys)
-    console.log(nameOfElement)
-    console.log(test[0])
-    console.log(test)
+    
 });
 
